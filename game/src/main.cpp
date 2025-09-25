6331661855ef38755bd0dbb393ec7dea9e07c34e
Rhieyanne <101554981@georgebrown.ca>
2025-09-25 13:36:28 -0400

    Removed Code, Removed Comments, Now the code is one big blob instead of seperating it into weeks

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -6,55 +6,6 @@
 // https://www.raylib.com/cheatsheet/cheatsheet.html
 
 
-/* THIS IS WEEK 1 CODE
-
-const unsigned int TARGET_FPS = 50;
-float time = 0;
-float dt;
-
-float x = 500;
-float y = 500;
-float frequency = 5.0f;
-float amplitude = 1;
-
-//Change world state
-void update() {
-
-	dt = 1.0 / TARGET_FPS;
-	time += dt;
-    y = y + (cos(time * frequency)) * frequency * amplitude * dt;
-    x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
-}
-
-//Display world State
-void draw() {
-    BeginDrawing();
-    ClearBackground(BLACK);
-    DrawText("Rhieyanne Fajardo: 101554981", 10, 80, 20, BLUE);
-    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-	DrawText(TextFormat("FPS: %i, TIME: %.2f", TARGET_FPS, time), GetScreenWidth() - 400, 40, 15, BLUE);
-	DrawCircle(x, y, 50, RED);
-    DrawCircle(GetScreenWidth()/2 + cos(time * frequency) * amplitude, GetScreenHeight()/2 * sin(time * frequency) * amplitude, 60, GREEN);
-    EndDrawing();
-}
-
-int main()
-{
-    InitWindow(InitialWidth, InitialHeight, "Rhieyanne-Fajardo-101554981");
-    SetTargetFPS(TARGET_FPS);
-
-    while (!WindowShouldClose())
-    {
-        update();
-		draw();
-    }
-
-    CloseWindow();
-    return 0;
-} */
-
-
-// THIS IS WEEK 2 CODE
 const unsigned int TARGET_FPS = 60;
 float dt = 1.0f / TARGET_FPS;
 float time;
@@ -70,17 +21,26 @@ Vector2 gravityAcceleration = { 0, 100 };
 
 void Update()
 {
-	// dt = 1.0f / TARGET_FPS;
-    // time += dt;
-    /* WORK IN PROGRESSif (IsKeyPressed(KEY_SPACE))
+	 dt = 1.0f / TARGET_FPS;
+    time += dt;
+    if (IsKeyPressed(KEY_SPACE))
     {
         //Drawing the Line
         position = { positionX, (float)GetScreenHeight() - positionY };
         velocity = { (float)cos(angle * DEG2RAD) * speed, (float)-sin(angle * DEG2RAD) * speed };
     }
 
-    /*position += position + velocity * dt;*/
+    //Velocity changes positiions over time. 
+	// Velocity = dispalcement / time
+	// Displacement = velocity * time 
+	position += velocity * dt;
+    
+	//Acceleration changes velocity over time.
+	// accel = delta velocity / time
+	// delta velocity = accel * time
+	velocity += gravityAcceleration * dt;
 }
+  
 void Draw()
 {   
     
@@ -94,10 +54,14 @@ void Draw()
     GuiSliderBar(Rectangle{ 10, 60, 200, 20 }, "", TextFormat("Angle: %.0f", angle), &angle, -180, 180); 
     GuiSliderBar(Rectangle{ 10, 80, 200, 20 }, "", TextFormat("Position X: %.0f", positionX), &positionX, 0, 300);
     GuiSliderBar(Rectangle{ 10, 100, 200, 20 }, "", TextFormat("Position Y: %.0f", positionY), &positionY, 0, 300);
+    GuiSliderBar(Rectangle{ 10, 140, 200, 20 }, "", TextFormat("Gravity Acceleration: %.0f", gravityAcceleration.y), &gravityAcceleration.y, -300, 300);
 
     //Drawing the Line
     Vector2 startPos = { positionX, GetScreenHeight() - positionY };
     Vector2 velocity = { (float)cos(angle * DEG2RAD) * speed, (float)-sin(angle * DEG2RAD) * speed };
+
+	//Drawing the Circle
+	DrawCircle(position.x, position.y, 15, RED);
 
 	DrawLineEx(startPos, startPos + velocity, 3, RED);
 

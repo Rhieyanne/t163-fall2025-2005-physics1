4e0d6a7f52c1799e9a1275d5ccd7bd17c21b1724
Rhieyanne <101554981@georgebrown.ca>
2025-10-13 15:00:11 -0400

    Forgot to add ball. to velocity as well

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -192,7 +192,7 @@ void Draw()
     //void DrawCircleV(Vector2 center, float radius, Color color); // Draw a color-filled circle (Vector version)
     for (auto& ball : sim.balls) {
         DrawCircleV(ball.position, 10, ball.randomColor);
-		DrawLineEx(ball.position, ball.position + velocity, 1, ball.randomColor);
+		DrawLineEx(ball.position, ball.position + ball.velocity, 1, ball.randomColor);
     }
 
     DrawLineEx(startPos, startPos + velocity, 3, RED);
